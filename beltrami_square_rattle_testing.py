ca4a2572d5e669c1a28b16850c94d0e72ff6fd6a
Ben Snowball <bss16@ic.ac.uk>
2017-11-29 14:35:20 +0000

    Added in another test that is used to obtain a plot for my presentation

--- beltrami_square_rattle_testing.py
+++ beltrami_square_rattle_testing.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     parser = ArgumentParser(description="""Execute the beltrami flow simulation using RATTLE""")
     parser.add_argument("--verbose", action="store_true",
                         help="Run while outputting some diagnostic info.")
-    parser.add_argument("run_number", type=int, nargs=1, choices=[0],
+    parser.add_argument("run_number", type=int, nargs=1, choices=[0, 1],
                         help="The number corresponding the parameters to be chosen.")
 
     args = parser.parse_args()
@@ -28,6 +28,20 @@ if __name__ == "__main__":
         c_scaling = 1.
         nt = 250
         N = 1000
+        bname = "results/beltrami-square-rattle/N=%d-endt=%g-nt=%d-dt=%g-c_scaling=%g" % (N, t, nt, t/nt, c_scaling)
+        ensure_dir(bname)
+        square = np.array([[-0.5,-0.5],[-0.5,0.5],[0.5,0.5],[0.5,-0.5]])
+        bbox = [-0.5, -0.5, 0.5, 0.5]
+        dens = ma.Density_2(square)
+
+        X = ma.optimized_sampling_2(dens, N, niter=1)
+        errorL2 = beltrami_rattle(X, dens, bbox, N=N, t=t, nt=nt, c_scaling=c_scaling, bname=bname, plot=True)[-1]
+
+    if run_number == 1:
+        t = 1.
+        c_scaling = 1.
+        nt = 10
+        N = 10
         bname = "results/beltrami-square-rattle/N=%d-endt=%g-nt=%d-dt=%g-c_scaling=%g" % (N, t, nt, t/nt, c_scaling)
         ensure_dir(bname)
         square = np.array([[-0.5,-0.5],[-0.5,0.5],[0.5,0.5],[0.5,-0.5]])
